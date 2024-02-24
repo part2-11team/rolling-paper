92b46b1cddf6bc2d10b2e78fb057a51e0dfd6b33
박지윤 <tkrhkfld123@naver.com>
2024-02-25 00:51:51 +0900

    ✅ [MOD] : Router 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import GlobalStyle from './style/GlobalStyle';
+import PaperListPage from './pages/PaperListPage';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <MainPage />
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/list" element={<PaperListPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
