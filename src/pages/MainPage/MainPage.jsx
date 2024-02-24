39f461023f22598c57f9b8f3f1880a4322c97a77
박지윤 <tkrhkfld123@naver.com>
2024-02-25 00:42:19 +0900

    ➕ [ADD] : 버튼 클릭 시 이벤트 함수

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import * as S from './MainPage.style.js';
+import Button from '../../components/Button';
 import Card1 from '../../assets/icon/card-img1.png';
 import Card2 from '../../assets/icon/card-img2.png';
 import Card3 from '../../assets/icon/card-img3.png';
 import Emoji from '../../assets/icon/Emoji.png';
 
 const MainPage = () => {
+  function handleMovetoListClick(e) {
+    e.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log('click');
+  }
+
   return (
     <S.MainPageDiv>
       <S.Section>
@@ -20,13 +27,13 @@ const MainPage = () => {
         <S.ImgContainer>
           <S.ImgBox>
             <S.SingleDiv>
-              <img src={Card1} alt={'카드1이미지'} />
+              <S.CoveredImg src={Card1} alt={'카드1이미지'} />
             </S.SingleDiv>
             <S.SingleDiv>
-              <img src={Card2} alt={'카드2이미지'} />
+              <S.CoveredImg src={Card2} alt={'카드2이미지'} />
             </S.SingleDiv>
             <S.SingleDiv>
-              <img src={Card3} alt={'카드3이미지'} />
+              <S.CoveredImg src={Card3} alt={'카드3이미지'} />
             </S.SingleDiv>
           </S.ImgBox>
         </S.ImgContainer>
@@ -42,10 +49,15 @@ const MainPage = () => {
         </S.DescriptionDiv>
         <S.ImgContainer>
           <S.EmojiDiv>
-            <img src={Emoji} alt={'이모지 이미지'} />
+            <S.CoveredImg src={Emoji} alt={'이모지 이미지'} />
           </S.EmojiDiv>
         </S.ImgContainer>
       </S.Section>
+      <S.ButtonContainer>
+        <Button size="full" onClick={(e) => handleMovetoListClick(e)}>
+          구경해보기
+        </Button>
+      </S.ButtonContainer>
     </S.MainPageDiv>
   );
 };
