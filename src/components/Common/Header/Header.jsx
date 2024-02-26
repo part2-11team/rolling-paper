9a9a9a60b81b53a6efb6b45db82eb02188321617
kimgaeun <kimgaeun21@naver.com>
2024-02-26 19:01:10 +0900

    💄 [UI] : header 스타일 적용

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,19 +1,20 @@
 ﻿import React from 'react';
-import style from './Header.module.css';
 import logo from '../../../assets/icon/logo.svg';
 import HeaderButton from './HeaderButton/HeaderButton';
-// import * as S from './Header.style.js';
+import * as Style from './Header.style.js';
 
 const Header = ({ page }) => {
-  if (page == 'main') {
+  if (page === 'main') {
     return (
-      <header>
-        <div>
-          <img src={logo} className={`${style.logo}`} alt="logo"></img>
-          <span>rolling</span>
-        </div>
-        <HeaderButton />
-      </header>
+      <Style.HeaderWrapper>
+        <Style.HeaderContent>
+          <Style.LogoDiv>
+            <Style.LogoImage src={logo} alt="logo" />
+            <Style.LogoText>rolling</Style.LogoText>
+          </Style.LogoDiv>
+          <HeaderButton />
+        </Style.HeaderContent>
+      </Style.HeaderWrapper>
     );
   }
 };
---
+++ src/components/Common/Header/Header.style.js
@@ -0,0 +1,43 @@
+﻿import styled from 'styled-components';
+import { COLORS } from '../../../style/colorPalette';
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  padding: 0 2.4rem;
+  width: 100%;
+  height: 64px;
+  justify-content: center;
+  align-items: center;
+  gap: 1px;
+  flex-shrink: 0;
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const LogoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const LogoImage = styled.img`
+  width: 27.818px;
+  height: 27.658px;
+`;
+
+export const LogoText = styled.span`
+  color: ${COLORS.Light_Gray_90};
+  text-align: center;
+  font-family: Poppins;
+  font-size: 19.971px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
