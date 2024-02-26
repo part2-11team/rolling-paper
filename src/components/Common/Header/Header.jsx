01fa0e863356d7a4a049f63c85a81d0d0e6ac619
kimgaeun <kimgaeun21@naver.com>
2024-02-26 17:48:27 +0900

    💄 [UI] : header 생성 및 로고 이미지 및 로고 추가

---
+++ src/components/Common/Header/Header.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import style from './Header.module.css';
+import logo from '../../../assets/icon/logo.svg';
+
+// import * as S from './Header.style.js';
+
+const Header = ({ page }) => {
+  if (page == 'main') {
+    return (
+      <header>
+        <div>
+          <img src={logo} className={`${style.logo}`} alt="logo"></img>
+          <span>rolling</span>
+        </div>
+      </header>
+    );
+  }
+};
+
+export default Header;
