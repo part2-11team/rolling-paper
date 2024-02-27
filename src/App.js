fd5865db23027f80fa5968f1f9d388d15c6cd599
armd482 <armd482@naver.com>
2024-02-27 17:03:31 +0900

    🔀 [MERGE] : develop 브랜치와의 MERGE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import GlobalStyle from './style/GlobalStyle';
+import PaperListPage from './pages/PaperListPage';
+import PostPage from './pages/PostPage';
 import PostIDPage from './pages/PostIDPage/PostIDPage';
-import React from 'react';
-import PaperListPage from './pages/PaperListPage/PaperListPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<MainPage />}></Route>
           <Route path="/list" element={<PaperListPage />}></Route>
           <Route path="/post">
+            <Route index element={<PostPage />}></Route>
             <Route path=":userID" element={<PostIDPage />}></Route>
           </Route>
         </Routes>
---
+++ src/components/Common/Header/Header.jsx
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import HeaderButton from './HeaderButton/HeaderButton';
+import LogoButton from './LogoButton/LogoButton';
+import * as Style from './Header.style.js';
+
+const Header = ({ page }) => {
+  if (page === 'main' || page === 'list') {
+    return (
+      <Style.HeaderWrapper>
+        <Style.HeaderContent>
+          <LogoButton to="/" />
+          <HeaderButton to="/post" />
+        </Style.HeaderContent>
+      </Style.HeaderWrapper>
+    );
+  } else {
+    return (
+      <Style.HeaderWrapper>
+        <Style.HeaderContent>
+          <LogoButton to="/" />
+        </Style.HeaderContent>
+      </Style.HeaderWrapper>
+    );
+  }
+};
+
+export default Header;
