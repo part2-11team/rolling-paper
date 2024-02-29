44c956440de4bfd401d71df469050d833871886e
이진호 <tead7@naver.com>
2024-02-29 10:10:02 +0900

    🔨 [FIX] : 프리티어 재설정 및 현재 개발 파일에 모든 형식 맞춤

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,25 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
   parserOptions: {
-    ecmaVersion: 2023,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['react'],
   rules: {
     'react/react-in-jsx-scopre': 'off',
     'react/prop-types': 0,
