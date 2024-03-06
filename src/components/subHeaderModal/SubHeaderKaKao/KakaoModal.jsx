243f115d90850521714c97a17ba6c2c4bfe4b49e
kimgaeun <kimgaeun21@naver.com>
2024-03-07 00:00:01 +0900

    🔨 [FIX]: kakao 모달 외부 click시 닫힘.

--- src/components/subHeaderModal/SubHeaderKaKao/KakaoModal.jsx
+++ src/components/subHeaderModal/SubHeaderKaKao/KakaoModal.jsx
@@ -1,11 +1,27 @@
-﻿import React from 'react';
+﻿import React, { useEffect, useRef } from 'react';
 import * as S from './KakaoModal.style';
 
-const KakaoModal = () => {
+const KakaoModal = ({ setKakaoOpen }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setKakaoOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [setKakaoOpen]);
+
   return (
-    <S.ModalWrap>
+    <S.ModalWrap ref={modalRef}>
       <h1>카카오 모달</h1>
     </S.ModalWrap>
   );
 };
+
 export default KakaoModal;
