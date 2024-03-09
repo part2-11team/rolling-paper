92d093fd5934ce7be9cd46a08c274cc608c3c178
박지윤 <tkrhkfld123@naver.com>
2024-03-09 17:56:34 +0900

     ✅ [MOD] : getRecipientsData api 합침

---
+++ src/useRequest.js
@@ -0,0 +1,36 @@
+import { useEffect, useState, useCallback } from 'react';
+import fetch from './fetch';
+
+function useRequest({ deps = [], skip = false, options }) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const refetch = useCallback(
+    async (...args) => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const { data: fetchedData } = await fetch({ ...options, ...args });
+        setData(() => fetchedData);
+        return { data: fetchedData };
+      } catch (err) {
+        setError(() => err);
+        return { error: err };
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [options?.url, options?.method, options?.data, options?.params],
+  );
+
+  useEffect(() => {
+    if (skip) return;
+    refetch();
+  }, deps);
+
+  return { data, isLoading, error, fetch: refetch };
+}
+
+export default useRequest;
